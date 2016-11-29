df6ddc0bd326ebf273105ec93b80d52c418281dd
Benjamin Joswig <benny.joswig1@web.de>
2016-11-29 09:23:10 +0100

    add alert map page, read todo-list

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Todo } from '../../models/todo';
+
 
 /*
   Generated class for the Map page.
@@ -13,10 +16,53 @@ import { NavController } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController) {}
+    todoList: Array<Todo>;
 
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
+
+        console.log('MapPage constructor');  
+
+  }
+
+  ionViewDidEnter() {
+
+        console.log('MapPage ViewDidEnter');
+
+
+        //Get list of todos from local storage
+        this.todoList = JSON.parse(localStorage.getItem("todos"));
+        if(!this.todoList) {
+            this.todoList = [];
+        }
+
+      console.log('length of list: '+ this.todoList.length);
+    
+      this.todoList.forEach(function(todo) {
+      // Set marker for every location in map
+      // check if there is more than one todo for a location
+      console.log('Title', todo.title);
+     
+      });
+      //console.log(this.todoList[0].location.latitude);
+
+  }
+
+  
   ionViewDidLoad() {
-    console.log('Hello Map Page');
+    console.log('MapPage ViewDidLoad');
+
+    // alert: short introduction to the user about the function of this page
+    this.showAlert();
+
+  }
+
+    showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Map View',
+      subTitle: 'This page gives you an overview where your to-dos are located! ',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
