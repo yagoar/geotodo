13508e61feba2ab8234cd518d4d0d53cb68e9cc6
Benjamin Joswig <benny.joswig1@web.de>
2016-11-29 09:37:47 +0100

    add leaflet map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { Todo } from '../../models/todo';
 
+import L from "leaflet";
 
 /*
   Generated class for the Map page.
@@ -17,6 +18,9 @@ import { Todo } from '../../models/todo';
 export class MapPage {
 
     todoList: Array<Todo>;
+    map: any;
+   center: {lat: number, lng: number};
+   marker: L.Marker;
 
   constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
@@ -54,6 +58,29 @@ export class MapPage {
     // alert: short introduction to the user about the function of this page
     this.showAlert();
 
+    //Munich
+     this.center = {lat: 48.137154, lng: 11.576124};
+ 
+     this.initMap();
+     // when no map, maybe to use this workaround:
+     //setTimeout(this.loadMap.bind(this), 100);
+
+  }
+
+  initMap() {
+     this.map = L.map('map', {
+       center: this.center,
+       zoom: 13
+    });
+
+     L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+       .addTo(this.map);
+ 
+     if (this.marker) {
+      this.marker = this.marker.setLatLng(this.center);
+     } else {
+       this.marker = L.marker(this.center).addTo(this.map);
+     }
   }
 
     showAlert() {
